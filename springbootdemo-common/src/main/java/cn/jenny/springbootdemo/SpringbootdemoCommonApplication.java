7a6718a4fd4dd962086ea8d737129ab42010fc53
liujing62 <liujing62@jd.com>
2018-11-29 11:44:39 +0800

    初始化项目

---
+++ springbootdemo-common/src/main/java/cn/jenny/springbootdemo/SpringbootdemoCommonApplication.java
@@ -0,0 +1,12 @@
+package cn.jenny.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoCommonApplication.class, args);
+	}
+}
